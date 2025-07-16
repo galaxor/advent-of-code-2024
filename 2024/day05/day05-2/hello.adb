230aace761417aaecb778bc9b2e5f4d5aae5aacc
Michelle Hughes <michelle@iheartmichelle.com>
2025-07-16 01:12:07 -0400

    I try to do the thing but it doesn't let me tamper with cursors!
    My whole plan was to move stuff around.  I guess I'll do it without the linked list, and just use a vector.

--- 2024/day05/day05-2/hello.adb
+++ 2024/day05/day05-2/hello.adb
@@ -20,22 +20,23 @@ procedure Hello is
   package Set_of_Naturals is new Ada.Containers.Ordered_Sets(Element_Type => Natural);
   use Set_of_Naturals;
 
-  Successor_Seen: Set_of_Naturals.Set := Set_of_Naturals.Empty_Set;
-
   package Map_Natural_to_Set_of_Naturals is new Ada.Containers.Ordered_Maps(Key_Type => Natural, Element_Type => Set_of_Naturals.Set, "=" => Set_of_Naturals."=");
   use Map_Natural_to_Set_of_Naturals;
 
-  Page_Predecessors: Map_Natural_to_Set_of_Naturals.Map := Map_Natural_to_Set_of_Naturals.Empty_Map;
+  Successor_Seen: Map_Natural_to_Set_of_Naturals.Map := Empty_Map;
+
+  Page_Predecessors: Map_Natural_to_Set_of_Naturals.Map := Empty_Map;
 
   Page_Map_Cursor: Map_Natural_to_Set_of_Naturals.Cursor := Map_Natural_to_Set_of_Naturals.No_Element;
 
   Inserted_Successfully: Boolean;
 
-  Current_Set: Set_of_Naturals.Set := Set_of_Naturals.Empty_Set;
+  Current_Set: Set_of_Naturals.Set := Empty_Set;
 
   package Linked_List_of_Naturals is new Ada.Containers.Doubly_Linked_Lists(Element_Type => Natural);
+  use Linked_List_of_Naturals;
   Page: Natural;
-  Pages: Linked_List_of_Naturals.List := Linked_List_of_Naturals.Empty_List;
+  Pages: Linked_List_of_Naturals.List := Empty_List;
   Pages_Cursor: Linked_List_of_Naturals.Cursor := Linked_List_of_Naturals.No_Element;
 
   Layout_is_Good: Boolean := true;
@@ -43,6 +44,13 @@ procedure Hello is
   Predecessor_Cursor: Set_of_Naturals.Cursor := Set_of_Naturals.No_Element;
 
   Sum: Natural := 0;
+
+  package Map_Natural_to_Cursor is new Ada.Containers.Ordered_Maps(Key_Type => Natural, Element_Type => Linked_List_of_Naturals.Cursor, "=" => Linked_List_of_Naturals."=");
+  use Map_Natural_to_Cursor;
+  Page_Catalog: Map_Natural_to_Cursor.Map := Map_Natural_to_Cursor.Empty_Map;
+  Page_Catalog_Cursor: Map_Natural_to_Cursor.Cursor := Map_Natural_to_Cursor.No_Element;
+
+  Successor_Page_Cursor: Linked_List_of_Naturals.Cursor;
 
 begin
 
@@ -101,6 +109,7 @@ begin
     Start_Index := 1;
 
     Pages := Linked_List_of_Naturals.Empty_List;
+    Page_Catalog := Map_Natural_to_Cursor.Empty_Map;
 
     loop
       Delimiter_Index := Index(Source => Line, Pattern => ",", From => Start_Index);
@@ -114,6 +123,9 @@ begin
       Page := Natural'Value(To_String(Token));
 
       Pages.Append(Page);
+      Pages_Cursor := Pages.Last;
+
+      Page_Catalog.Insert(Page, Pages_Cursor);
 
       Start_Index := Delimiter_Index+1;
 
@@ -126,26 +138,56 @@ begin
     -- We have what we need to figure out if this page layout is valid.
 
     Layout_is_Good := true;
-    Successor_Seen := Empty_Set;
+    Successor_Seen := Map_Natural_to_Set_of_Naturals.Empty_Map;
 
     for Pages_Cursor in Pages.Iterate loop
       Page := Linked_List_of_Naturals.Element(Pages_Cursor);
 
       -- If we've already seen a successor to this page, the layout is bad.
-      if Set_of_Naturals.Has_Element(Successor_Seen.Find(Page)) then
-        Layout_is_Good := false;
+      if Has_Element(Successor_Seen.Find(Page)) then
+        if Natural(Length(Element(Successor_Seen.Find(Page)))) > 0 then
+          Layout_is_Good := false;
+          -- This is where we move pages around.
+          -- We need to find the successor page that we've already seen, and we need to move it to the position in Pages after this page.
+          -- This page is number Page, and it's located at Pages_Cursor.
+          for Successor_Page in Element(Successor_Seen.Find(Page)).Iterate loop
+            -- Find a cursor pointing to the successor page, and then use the linked list Splice interface to move it to be after Pages_Cursor.
+            Successor_Page_Cursor := Element(Page_Catalog.Find(Element(Successor_Page)));
+
+            Splice(Source => Pages, Target => Pages, Position => Successor_Page_Cursor, Before => Next(Pages_Cursor));
+
+            Put("The list is now:");
+            for Cool_Page in Pages.Iterate loop
+              Put(Natural'Image(Element(Cool_Page)));
+            end loop;
+            Put_Line("");
+          end loop;
+        end if;
       end if;
       exit when not Layout_is_Good;
 
       -- We haven't seen a successor to this page yet, so let's mark all of
       -- this page's predecessors and tell them that a successor has been seen.
       Page_Map_Cursor := Page_Predecessors.Find(Page);
-      if Map_Natural_to_Set_of_Naturals.Has_Element(Page_Map_Cursor) then
-        Current_Set := Map_Natural_to_Set_of_Naturals.Element(Page_Predecessors.Find(Page));
+      if Has_Element(Page_Map_Cursor) then
+        Current_Set := Element(Page_Map_Cursor);
         for Predecessor_Cursor in Current_Set.Iterate loop
-          if not Has_Element(Successor_Seen.Find(Element(Predecessor_Cursor))) then 
-            Successor_Seen.Insert(Element(Predecessor_Cursor));
+          Predecessor_Page := Element(Predecessor_Cursor);
+          -- Insert this successor in the Successor_Seen set for this predecessor.
+          if not Has_Element(Successor_Seen.Find(Predecessor_Page)) then 
+            Successor_Seen.Insert(Predecessor_Page, Empty_Set);
           end if;
+
+          -- In order to add to a set within a map, we have to write a closure!
+          declare 
+            procedure Insert_Page(The_Key: in Natural; The_Set: in out Set_of_Naturals.Set) is
+            begin
+              The_Set.Insert(Page);
+            end Insert_Page;
+          begin
+            -- Now we use the closure using the cumbersome Update_Element API.
+            Successor_Seen.Update_Element(Successor_Seen.Find(Predecessor_Page), Insert_Page'Access);
+          end;
         end loop;
       end if;
       
